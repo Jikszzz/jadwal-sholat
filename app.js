f02e37658f2eb4232c9cf0ad71b8cec5fcf3588d
Jikszzz <zhicxiap99@gmail.com>
2023-04-18 16:18:16 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+function prayerTimes(latitude, longitude) {
+  fetch(
+    "http://api.aladhan.com/v1/calendar?latitude=" +
+      latitude +
+      "&longitude=" +
+      longitude +
+      "&method=2"
+  )
+    .then((response) => response.json())
+    .then(function (response) {
+      let date = new Date();
+      let today = date.getDate() - 1;
+      console.log(today);
+      console.log(response.data[today]);
+    });
+}
+
+function success(position) {
+  prayerTimes(position.coords.latitude, position.coords.longitude);
+}
+
+function error() {
+  alert("Posisi tidak dapat diakses");
+}
+
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation tidak di dukung");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+function index() {
+  let app = document.getElementById("app");
+  let h3 = document.createElement("h3");
+  h3.innerHTML = "Jadwal Sholat";
+  app.appendChild(h3);
+  userLocation();
+}
+index();
